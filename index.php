be0dc6c2a79496ae35d4a9f92e055e3d43af0618
Naufal Firaas <naufalfiraas09@gmail.com>
2022-11-17 12:00:25 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aperiam laboriosam eligendi, ut a ex earum ipsam, aliquam dicta fugit animi esse ullam rerum aliquid dolore illo maiores beatae magni, est sit exercitationem. Modi quasi nisi cupiditate quae nulla. Adipisci fugiat facere magni, dicta voluptatem porro aliquam sint reprehenderit, vel explicabo repellendus? Illum ducimus, modi quibusdam sequi doloremque in ut error nemo minus aspernatur laborum eius non, excepturi dolores, voluptatibus veritatis accusantium corporis incidunt adipisci laudantium. Pariatur modi tempora reiciendis expedita quia excepturi odio, quam error sint minima distinctio illum provident explicabo corrupti, autem quas laudantium, veritatis consequuntur dolores accusantium praesentium! Mollitia error totam voluptatem distinctio! Cum dignissimos quas perferendis sequi voluptas? Dicta odio consectetur accusantium, quia architecto soluta repellendus aliquam eum similique minus a natus ratione quod corrupti quisquam labore, incidunt amet debitis, nihil ex mollitia consequatur. Sit suscipit aperiam natus libero hic praesentium repellat quia labore blanditiis fuga. Molestiae reprehenderit possimus iure quidem incidunt.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
